0d8ff175c9100bff7e504905879ffe955349bfb1
Illensky <alexis.laroche.02240@gmail.com>
2022-01-17 16:39:25 +0100

    exercice done

--- index.php
+++ index.php
@@ -6,7 +6,10 @@
  * --> Attention à bien fermer votre fichier si vous utilisez fopen()
  */
 // TODO Votre code ici.
-
+$string = "du texte lorem ( 1 seule ligne suffit )";
+$file = fopen("string.txt", "xb");
+fwrite($file, $string);
+fclose($file);
 
 /**
  * 3. Créez un tableau contenant au moins 10 chaînes de caractère au choix.
@@ -15,13 +18,35 @@
  * --> Attention: les chaînes de caractères doivent s'ajouter à la suite du contenu déjà existant, pas d'écrasement.
  */
 // TODO Votre code ici
+$stringArray = [
+    "chaînes de caractère au choix",
+    "chaînes de caractère au choix",
+    "chaînes de caractère au choix",
+    "chaînes de caractère au choix",
+    "chaînes de caractère au choix",
+    "chaînes de caractère au choix",
+    "chaînes de caractère au choix",
+    "chaînes de caractère au choix",
+    "chaînes de caractère au choix",
+    "chaînes de caractère au choix",
+    "chaînes de caractère au choix"
+];
 
+$file = fopen("string.txt", "ab");
+
+foreach ($stringArray as $string) {
+    fwrite($file, $string);
+}
+
+fclose($file);
 
 /**
  * 5. Trouvez une solution pour afficher à l'aide d'un simple echo l'extension du fichier index.php
  */
 // TODO Votre code ici
 
+$info = pathinfo("index.php");
+echo $info['extension'];
 
 
 /**
@@ -29,10 +54,16 @@
  */
 // TODO Votre code ici.
 
-
+if (!file_exists("toto")) {
+    echo "Le fichier 'toto' n'existe pas";
+}
 /**
  * Super bonus.
  * Parcourrez votre fichier, à chaque fois que vous rencontrez le caractère 'a', remplacez le par le caractère '@'
  * Attention, il y a un piège avec les pointeurs, et une manière très simple de procéder... réfléchissez...
  */
 // TODO Votre code ici si vous faites le bonus.
+
+$str = file_get_contents("string.txt");
+$str = str_replace("a", "@", $str);
+file_put_contents("string.txt",$str);
